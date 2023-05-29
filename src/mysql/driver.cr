054f57610ea1acc89e75e81269ff3d2bf221e45f
Brian J. Cardiff <bcardiff@gmail.com>
2023-05-29 17:00:30 -0300

    Refactor connection builder

--- src/mysql/driver.cr
+++ src/mysql/driver.cr
@@ -1,6 +1,9 @@
 class MySql::Driver < DB::Driver
-  def build_connection(context : DB::ConnectionContext) : MySql::Connection
-    MySql::Connection.new(context)
+  def connection_builder(uri : URI) : Proc(::DB::Connection)
+    params = HTTP::Params.parse(uri.query || "")
+    options = connection_options(params)
+    mysql_options = MySql::Connection::Options.from_uri(uri)
+    ->{ MySql::Connection.new(options, mysql_options).as(::DB::Connection) }
   end
 end
 
